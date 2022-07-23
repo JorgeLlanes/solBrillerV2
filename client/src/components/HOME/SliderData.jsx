75382a427fbef48def4a843ac433deaca6e7f00e
jnrlns <llanesjorge262@gmail.com>
2022-07-22 19:53:14 -0500

    Added pics/Slider re-order

--- client/src/components/HOME/SliderData.jsx
+++ client/src/components/HOME/SliderData.jsx
@@ -1,16 +1,31 @@
 export const SliderData = [
     {
-        image: require('../../img/pic4.jpeg')
+        image: require('../../img/pic_1.jpeg')
     },
     {
-        image: require('../../img/pic3.jpeg')
+        image: require('../../img/pic_2.jpg')
     },
     {
-        image: require("../../img/pic1.jpg")
+        image: require("../../img/pic_4.jpg")
 
     },
     {
-        image: require("../../img/pic2.jpeg")
+        image: require("../../img/pic_5.jpeg")
+    },
+    {
+        image: require("../../img/pic_6.jpeg")
+    },
+    {
+        image: require("../../img/pic_3.jpg")
+    },
+    {
+        image: require("../../img/pic_7.jpeg")
+    },
+    {
+        image: require("../../img/pic_9.jpeg")
+    },
+    {
+        image: require("../../img/pic_8.jpeg")
     }
 ]
 
